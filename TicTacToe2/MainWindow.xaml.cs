26f3a43a20a1a8371603865bd7621598941909d2
COLIN\User <punkmuppet@gmail.com>
2019-05-01 01:08:16 +0100

    Everything is working and the layout is looking okay.
    Just need to add a computer opponent.

--- TicTacToe2/MainWindow.xaml.cs
+++ TicTacToe2/MainWindow.xaml.cs
@@ -20,9 +20,210 @@ namespace TicTacToe2
     /// </summary>
     public partial class MainWindow : Window
     {
+        string xwins = ("X = " + Properties.Settings.Default.XScore + " wins");
+        string owins = ("O = " + Properties.Settings.Default.OScore + " wins");
+        string currentPlayer = "X";
+
         public MainWindow()
         {
             InitializeComponent();
+            //if (Properties.Settings.Default.XScore < 1)
+            //{
+            //    Properties.Settings.Default.XScore = 0;
+            //}
+            //if (Properties.Settings.Default.OScore < 1)
+            //{
+            //    Properties.Settings.Default.OScore = 0;
+            //}
+            //Properties.Settings.Default.Save();
+
+            XScores.Text = xwins;
+            OScores.Text = owins;
+            CurrentPlayer.Text = (currentPlayer + " to play...");
+        }
+
+        private void WinCheck()
+        {
+            if ((Box1.Content.ToString() == "X" && Box2.Content.ToString() == "X" && Box3.Content.ToString() == "X")
+                || (Box4.Content.ToString() == "X" && Box5.Content.ToString() == "X" && Box6.Content.ToString() == "X")
+                || (Box7.Content.ToString() == "X" && Box8.Content.ToString() == "X" && Box9.Content.ToString() == "X")
+                || (Box1.Content.ToString() == "X" && Box4.Content.ToString() == "X" && Box7.Content.ToString() == "X")
+                || (Box2.Content.ToString() == "X" && Box5.Content.ToString() == "X" && Box8.Content.ToString() == "X")
+                || (Box3.Content.ToString() == "X" && Box6.Content.ToString() == "X" && Box9.Content.ToString() == "X")
+                || (Box1.Content.ToString() == "X" && Box5.Content.ToString() == "X" && Box9.Content.ToString() == "X")
+                || (Box3.Content.ToString() == "X" && Box5.Content.ToString() == "X" && Box7.Content.ToString() == "X")
+                || (Box1.Content.ToString() == "O" && Box2.Content.ToString() == "O" && Box3.Content.ToString() == "O")
+                || (Box4.Content.ToString() == "O" && Box5.Content.ToString() == "O" && Box6.Content.ToString() == "O")
+                || (Box7.Content.ToString() == "O" && Box8.Content.ToString() == "O" && Box9.Content.ToString() == "O")
+                || (Box1.Content.ToString() == "O" && Box4.Content.ToString() == "O" && Box7.Content.ToString() == "O")
+                || (Box2.Content.ToString() == "O" && Box5.Content.ToString() == "O" && Box8.Content.ToString() == "O")
+                || (Box3.Content.ToString() == "O" && Box6.Content.ToString() == "O" && Box9.Content.ToString() == "O")
+                || (Box1.Content.ToString() == "O" && Box5.Content.ToString() == "O" && Box9.Content.ToString() == "O")
+                || (Box3.Content.ToString() == "O" && Box5.Content.ToString() == "O" && Box7.Content.ToString() == "O")
+                )
+            {
+                MessageBox.Show("Game won by player " + currentPlayer + "!");
+
+                if (currentPlayer == "X")
+                {
+                    Properties.Settings.Default.XScore++;
+                }
+                else if (currentPlayer == "O")
+                {
+                    Properties.Settings.Default.OScore++;
+                }
+                Properties.Settings.Default.Save();
+                ResetGame();
+            }
+            else if (Box1.Content.ToString() != " "
+                && Box2.Content.ToString() != " "
+                && Box3.Content.ToString() != " "
+                && Box4.Content.ToString() != " "
+                && Box5.Content.ToString() != " "
+                && Box6.Content.ToString() != " "
+                && Box7.Content.ToString() != " "
+                && Box8.Content.ToString() != " "
+                && Box9.Content.ToString() != " ")
+            {
+                MessageBox.Show("The game resulted in a tie!");
+                ResetGame();
+            }
+
+            else
+            {
+                PlayerSwitch();
+            }
+        }
+
+        void PlayerSwitch()
+        {
+            if (currentPlayer == "X")
+            {
+                currentPlayer = "O";
+                OScores.FontWeight = FontWeights.Bold;
+                XScores.FontWeight = FontWeights.Light;
+            }
+
+            else
+            {
+                currentPlayer = "X";
+                XScores.FontWeight = FontWeights.Bold;
+                OScores.FontWeight = FontWeights.Light;
+            }
+
+            CurrentPlayer.Text = (currentPlayer + " to play...");
+        }
+
+        void ResetGame()
+        {
+            Box1.IsEnabled = true;
+            Box2.IsEnabled = true;
+            Box3.IsEnabled = true;
+            Box4.IsEnabled = true;
+            Box5.IsEnabled = true;
+            Box6.IsEnabled = true;
+            Box7.IsEnabled = true;
+            Box8.IsEnabled = true;
+            Box9.IsEnabled = true;
+            Box1.Content = " ";
+            Box2.Content = " ";
+            Box3.Content = " ";
+            Box4.Content = " ";
+            Box5.Content = " ";
+            Box6.Content = " ";
+            Box7.Content = " ";
+            Box8.Content = " ";
+            Box9.Content = " ";
+            string xwins = ("X = " + Properties.Settings.Default.XScore + " wins");
+            string owins = ("O = " + Properties.Settings.Default.OScore + " wins");
+            XScores.Text = xwins;
+            OScores.Text = owins;
+        }
+
+        private void Box1_Click(object sender, RoutedEventArgs e)
+        {
+            Box1.IsEnabled = false;
+            Box1.Content = currentPlayer;
+            WinCheck();
+        }
+
+        private void Box2_Click(object sender, RoutedEventArgs e)
+        {
+            Box2.IsEnabled = false;
+            Box2.Content = currentPlayer;
+            WinCheck();
+        }
+
+        private void Box3_Click(object sender, RoutedEventArgs e)
+        {
+            Box3.IsEnabled = false;
+            Box3.Content = currentPlayer;
+            WinCheck();
+        }
+
+        private void Box4_Click(object sender, RoutedEventArgs e)
+        {
+            Box4.IsEnabled = false;
+            Box4.Content = currentPlayer;
+            WinCheck();
+        }
+
+        private void Box5_Click(object sender, RoutedEventArgs e)
+        {
+            Box5.IsEnabled = false;
+            Box5.Content = currentPlayer;
+            WinCheck();
+        }
+
+        private void Box6_Click(object sender, RoutedEventArgs e)
+        {
+            Box6.IsEnabled = false;
+            Box6.Content = currentPlayer;
+            WinCheck();
+        }
+
+        private void Box7_Click(object sender, RoutedEventArgs e)
+        {
+            Box7.IsEnabled = false;
+            Box7.Content = currentPlayer;
+            WinCheck();
+        }
+
+        private void Box8_Click(object sender, RoutedEventArgs e)
+        {
+            Box8.IsEnabled = false;
+            Box8.Content = currentPlayer;
+            WinCheck();
+        }
+
+        private void Box9_Click(object sender, RoutedEventArgs e)
+        {
+            Box9.IsEnabled = false;
+            Box9.Content = currentPlayer;
+            WinCheck();
+        }
+
+        private void ResetScores_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Do you really want to clear the scores?",
+                                          "Confirmation",
+                                          MessageBoxButton.YesNo,
+                                          MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
+            {
+                Properties.Settings.Default.XScore = 0;
+                Properties.Settings.Default.OScore = 0;
+                Properties.Settings.Default.Save();
+                string xwins = ("X = " + Properties.Settings.Default.XScore + " wins");
+                string owins = ("O = " + Properties.Settings.Default.OScore + " wins");
+                XScores.Text = xwins;
+                OScores.Text = owins;
+            }
+        }
+
+        private void ResetGame_Click(object sender, RoutedEventArgs e)
+        {
+            ResetGame();
         }
     }
 }
--- TicTacToe2/Properties/Settings.Designer.cs
+++ TicTacToe2/Properties/Settings.Designer.cs
@@ -8,22 +8,42 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace TicTacToe2.Properties
-{
-
-
+namespace TicTacToe2.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public int XScore {
+            get {
+                return ((int)(this["XScore"]));
+            }
+            set {
+                this["XScore"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public int OScore {
+            get {
+                return ((int)(this["OScore"]));
+            }
+            set {
+                this["OScore"] = value;
             }
         }
     }
